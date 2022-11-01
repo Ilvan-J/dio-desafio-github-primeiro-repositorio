2b6a73bc01437e7209ac917e2c55fb0ff5905da7
Ilvan-J <ilvanlopesjardim110@gmail.com>
2022-11-01 15:19:09 -0300

    correção

--- 2-Dominando a Linguagem de Programação Java/2-3-Trabalhando com Collections Java/curso-dio-intro-collections/src/br/com/dio/collections/set/ExemploOrdenacaoSet.java
+++ 2-Dominando a Linguagem de Programação Java/2-3-Trabalhando com Collections Java/curso-dio-intro-collections/src/br/com/dio/collections/set/ExemploOrdenacaoSet.java
@@ -1,2 +1,50 @@
-package br.com.dio.collections.set;public class ExemploOrdenacaoSet {
+package br.com.dio.collections.set;
+
+import java.util.*;
+
+public class ExemploOrdenacaoSet {
+    /*
+    * Dados os seguintes informações sobre minhas séries favoritas,
+    * crie um conjunto e ordene este conjunto exibindo:
+    * (nome - genero - tempo de episódio):
+    *
+    * Serie 1 = Nome: got, gênero: fantasia, tempoEpisodio: 60
+    * Serie 2 = Nome: dark, gênero: drama, tempoEpisodio: 60
+    * Serie 3 = Nome: that '70 show, gênero: comédia, tempoEpisodio: 25
+    * */
+    public static void main(String[] args) {
+        System.out.println("--\tOrdem aleatória\t--");
+        Set<Serie> minhasSeries = new HashSet<>(){{
+            add(new Serie("got", "fantasia", 60));
+            add(new Serie("dark", "drama", 60));
+            add(new Serie("that '70 show", "comédia", 25));
+        }};
+
+        for (Serie serie: minhasSeries) System.out.println(serie.getNome() + " - "
+        + serie.getGenero() + " - " + serie.getTempoEpisodio());
+
+        System.out.println("--\tOrdem inserção\t--");
+
+        Set<Serie> minhasSeries1 = new LinkedHashSet<>() {{
+            add(new Serie("got", "fantasia", 60));
+            add(new Serie("dark", "drama", 60));
+            add(new Serie("that '70 show", "comédia", 25));
+        }};
+
+        for (Serie serie: minhasSeries1) System.out.println(serie.getNome() + " - "
+        + serie.getGenero() + " - " + serie.getTempoEpisodio());
+
+        System.out.println("--\tOrdem natural (temporEpisodio)\t--");
+        Set<Serie> minhasSeries2 = new TreeSet<>(minhasSeries1);
+        for (Serie serie: minhasSeries2) System.out.println(serie.getNome() + " - "
+                + serie.getGenero() + " - " + serie.getTempoEpisodio());
+
+        System.out.println("--\tOrdem Nome/Gênero/tempoEpisodio\t--");
+
+        Set<Serie> minhasSeries3 = new TreeSet<>(new ComparatorNomeGeneroTempoEpisodio());
+        minhasSeries3.addAll(minhasSeries);
+        for (Serie serie: minhasSeries3) System.out.println(serie.getNome() + " - "
+                + serie.getGenero() + " - " + serie.getTempoEpisodio());
+
+    }
 }
--- 2-Dominando a Linguagem de Programação Java/2-3-Trabalhando com Collections Java/curso-dio-intro-collections/src/br/com/dio/collections/set/Serie.java
+++ 2-Dominando a Linguagem de Programação Java/2-3-Trabalhando com Collections Java/curso-dio-intro-collections/src/br/com/dio/collections/set/Serie.java
@@ -1,2 +1,58 @@
-package br.com.dio.collections.set;public class Serie {
+package br.com.dio.collections.set;
+
+import java.util.Objects;
+
+public class Serie implements Comparable<Serie>{
+    private String nome;
+    private String genero;
+    private Integer tempoEpisodio;
+
+    public Serie(String nome, String genero, Integer tempoEpisodio) {
+        this.nome = nome;
+        this.genero = genero;
+        this.tempoEpisodio = tempoEpisodio;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public Integer getTempoEpisodio() {
+        return tempoEpisodio;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "nome='" + nome + '\'' +
+                ", genero='" + genero + '\'' +
+                ", tempoEpisodio=" + tempoEpisodio +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {https://www.linkedin.com/in/ilvan-lopes-jardim
+    if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Serie serie = (Serie) o;
+        return nome.equals(serie.nome) && genero.equals(serie.genero) && tempoEpisodio.equals(serie.tempoEpisodio);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, genero, tempoEpisodio);
+    }
+
+    @Override
+    public int compareTo(Serie serie) {
+        int tempoEspisodio = Integer.compare(this.getTempoEpisodio(), serie.getTempoEpisodio());
+
+        if (tempoEspisodio != 0) return tempoEspisodio;
+
+        return this.getGenero().compareTo(serie.getGenero());
+    }
 }
